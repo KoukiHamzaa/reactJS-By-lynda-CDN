cf208f870ac1826798d89de3d8358ed43a2aa1a0
HamzaKouki <hamzakouki33@gmail.com>
2020-03-23 13:49:58 +0100

    newCommit

--- ReactApp/Exercise Files/ch02/start/react-is-fun/src/index.js
+++ ReactApp/Exercise Files/ch02/start/react-is-fun/src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDom from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+var style = {
+  backgroundColor : 'orange',
+  color : 'white',
+  fontFamily : 'Arial',
+}
+const title = React.createElement(
+  "ul",
+  { id: "title", className: "header", style },
+  React.createElement(
+    'li',
+    {},
+    'item on our list'
+  )
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDom.render(
+  title,
+  document.getElementById("root")
+)
