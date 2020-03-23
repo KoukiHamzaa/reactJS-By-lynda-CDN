84bbff362fbac9bb57b4161169a78a1c5eeeb05f
HamzaKouki <hamzakouki33@gmail.com>
2020-03-23 14:38:31 +0100

    newCommit

--- ReactApp/Exercise Files/ch02/start/react-is-fun/src/index.js
+++ ReactApp/Exercise Files/ch02/start/react-is-fun/src/index.js
@@ -1,19 +1,76 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDom from "react-dom";
 
+let skiData = {
+  total: 50,
+  powder: 20,
+  backcountry: 10,
+  goal: 100
+};
 
-class Message extends React.Component {
-  render() {
-    return (
+
+
+const SkiDayCounter = ({total, powder, backcountry, goal}) => {
+const getPercent = decimal => {
+    return decimal * 100 + "%";
+  };
+  
+const calcGoalProgress = (total, goal) => {
+    return getPercent(total / goal);
+  };
+  return (
+      <section>
       <div>
-        <h1 style={{ color: this.props.color }}>{this.props.msg}</h1>
-        <p>I'll check back in {this.props.minutes} minutes</p>
+        <p>Total Days : {total}</p>
       </div>
-    );
-  }
+      <div>
+        <p>Powder Days : {powder}</p>
+      </div>
+      <div>
+        <p>Backcountry Days : {backcountry}</p>
+      </div>
+      <div>
+        <p>Goal Progress  : {calcGoalProgress(total, goal)}</p>
+      </div>
+    </section>
+  )
 }
 
+
+// class SkiDayCounter extends Component {
+//   getPercent = decimal => {
+//     return decimal * 100 + "%";
+//   };
+//   calcGoalProgress = (total, goal) => {
+//     return this.getPercent(total / goal);
+//   };
+//   render() {
+//     const { total, powder, backcountry, goal } = this.props;
+//     return (
+//       <section>
+//         <div>
+//           <p>Total Days : {total}</p>
+//         </div>
+//         <div>
+//           <p>Powder Days : {powder}</p>
+//         </div>
+//         <div>
+//           <p>Backcountry Days : {backcountry}</p>
+//         </div>
+//         <div>
+//           <p>Goal Progress  : {this.calcGoalProgress(total, goal)}</p>
+//         </div>
+//       </section>
+//     );
+//   }
+// }
+
 ReactDom.render(
-  <Message minutes={5} color="blue" msg="how are u !" />,
+  <SkiDayCounter
+    total={skiData.total}
+    powder={skiData.powder}
+    backcountry={skiData.backcountry}
+    goal={skiData.goal}
+  />,
   document.getElementById("root")
 );
