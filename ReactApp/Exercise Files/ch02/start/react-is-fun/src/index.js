befd358a7ea50b39185ddec16888f398540c5577
HamzaKouki <hamzakouki33@gmail.com>
2020-03-23 14:02:39 +0100

    newCommit

--- ReactApp/Exercise Files/ch02/start/react-is-fun/src/index.js
+++ ReactApp/Exercise Files/ch02/start/react-is-fun/src/index.js
@@ -2,22 +2,18 @@ import React from "react";
 import ReactDom from "react-dom";
 
 
-var style = {
-  backgroundColor : 'orange',
-  color : 'white',
-  fontFamily : 'Arial',
+class Message extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 style={{ color: this.props.color }}>{this.props.msg}</h1>
+        <p>I'll check back in {this.props.minutes} minutes</p>
+      </div>
+    );
+  }
 }
-const title = React.createElement(
-  "ul",
-  { id: "title", className: "header", style },
-  React.createElement(
-    'li',
-    {},
-    'item on our list'
-  )
-);
 
 ReactDom.render(
-  title,
+  <Message minutes={5} color="blue" msg="how are u !" />,
   document.getElementById("root")
-)
+);
