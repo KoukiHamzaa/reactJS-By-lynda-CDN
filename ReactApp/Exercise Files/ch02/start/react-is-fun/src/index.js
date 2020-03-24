411e65bf326d4222e92776e74c79c5468f292dc0
HamzaKouki <hamzakouki33@gmail.com>
2020-03-24 18:27:33 +0100

    newCommit

--- ReactApp/Exercise Files/ch02/start/react-is-fun/src/index.js
+++ ReactApp/Exercise Files/ch02/start/react-is-fun/src/index.js
@@ -1,28 +1,40 @@
 import React, { Component } from "react";
 import ReactDom from "react-dom";
+import { render } from "react-dom";
+
+let BookList = [
+  { title: "Life of Me", auther: "Hamza Kouki", pages: 50 },
+  { title: "Kitchen Congrats", auther: "Hamza Kouki", pages: 100 },
+  { title: "You Have To Try Me", auther: "Hamza Kouki", pages: 150 }
+];
 
 const Book = ({ title, auther, pages }) => {
   return (
     <section>
-      <h1>{title}</h1>
-      <p>{auther}</p>
-      <p>{pages}</p>
+      <h1> Title : {title}</h1>
+      <p>Auther : {auther}</p>
+      <p>Pages : {pages}</p>
     </section>
   );
 };
 
-const Library = () => {
+const Library = ({books})  => {
   return (
     <div>
-      Welcome
-      <Book title="Hamza Life" auther="Hamza Kouki" pages={500} />
+      {books.map((book,key) => (
+        <Book 
+        title={book.title}
+        auther={book.auther}
+        pages={book.pages} 
+        key={key}/>
+      ))}
     </div>
   );
 };
 
 ReactDom.render(
   <div>
-    <Library />
+    <Library books={BookList} />
   </div>,
   document.getElementById("root")
 );
